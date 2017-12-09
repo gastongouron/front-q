347bc17c2bef2ff4cfd5e6d830cf5b1613e762fe
paulgastongouron <paul-gaston.gouron@paymium.com>
2017-12-09 11:47:09 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
+
 import {
   Route,
   NavLink,
   HashRouter
 } from "react-router-dom";
+
 import Home from "./Home";
 import Transactions from "./Transactions";
 
@@ -13,15 +16,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/Transactions">Transactions</a></li>
-          </ul>
-        <div className="content">
+      <HashRouter>
+        <div>
+          <div class="container">
+              <div class="left">
+                <div className="logo">
+                   <h1>Finpal</h1>
+                </div>
+                  <ul className="header">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/transactions">Transactions</NavLink></li>
+                  </ul>
+              </div>
+              <div class="right">
+                <div className="bar"></div>
+                <div className="content">
+                  <Route exact path="/" component={Home}/>
+                  <Route path="/transactions" component={Transactions}/>
+                </div>
+              </div>
+          </div>
         </div>
-    </div>
+      </HashRouter>
     );
   }
 }
--- src/Transactions.js
+++ src/Transactions.js
@@ -1,20 +1,53 @@
 import React, { Component } from "react";
+import ReactTable from 'react-table'
 
 class Transactions extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { data: [] }
+  }
+
+  loadData() {
+    fetch('http://private-5d708-interviewfront.apiary-mock.com/transactions')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({data: data })
+        console.log(this.state.data)
+    })
+      .catch(err => console.error(this.props.url, err.toString()))
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+
+
   render() {
     return (
-      <div>
-        <h2>STUFF</h2>
-        <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
-      </div>
+
+
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>DD-MM-YY</th>
+              <th>CounterParty Name</th>
+              <th>Payment type</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+              <td>Germany</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+
     );
   }
 }
