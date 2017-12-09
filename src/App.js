56ccffa71b310cbfe97e29aef21c274d1c4ce221
paulgastongouron <paul-gaston.gouron@paymium.com>
2017-12-09 11:01:49 +0100

    Add components and routing for home & trnsactions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Transactions from "./Transactions";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><a href="/">Home</a></li>
+            <li><a href="/Transactions">Transactions</a></li>
+          </ul>
+        <div className="content">
+        </div>
+    </div>
     );
   }
 }
